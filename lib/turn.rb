0415ee5935af4d05b34fedf4aef5dbddd16ac5ff
Joshua Clarence Sadsad <joshsad2@gmail.com>
2017-12-07 10:05:45 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,19 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]}  "
 end
 
+def valid_move?(board, index)
+  if index.between?(0, 8)
+end
+
+def position_taken?(board, index)
+  (board[index] == "" || board[index] == " " || board[index] == nil) ?
+  false : true
+end
+
+def move(board, index, token)
+end
+
+
 def turn(board)
   puts "Please enter 1 - 9:"
 end
