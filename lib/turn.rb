d261e541f9b556834ff6ef601f0c4de9c28f72f6
Joshua Clarence Sadsad <joshsad2@gmail.com>
2017-12-07 10:18:47 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -33,7 +33,7 @@ def turn(board)
   index = input_to_index(input)
   if valid_move?(board, index)
     move(board, index)
-    display_board
+    display_board(board)
   else
     puts "Please enter a VALID input."
   end
